77e0361b67281fb40330a824f99ff675267dc88d
Fantiko <86378890+Fantiko@users.noreply.github.com>
2023-07-07 18:37:04 -0300

    Shortest Job First completo

---
+++ src/main/java/com/github/rok/algorithm/ShortestJobFirst.java
@@ -0,0 +1,23 @@
+package com.github.rok.algorithm;
+
+import com.github.rok.interfaces.IMainController;
+import com.github.rok.interfaces.AlgorithmInterface;
+import com.github.rok.os.Process;
+
+public class ShortestJobFirst implements AlgorithmInterface{
+
+    private final IMainController controller;
+
+    public ShortestJobFirst(IMainController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void execute() {
+        Process lowestSizeProcess = controller.getIMemory().getLowestTimeProcess();
+        if (lowestSizeProcess == null) {
+            return;
+        }
+        controller.addProcessToCPU(lowestSizeProcess, lowestSizeProcess.getProcessTime());
+    }
+}
