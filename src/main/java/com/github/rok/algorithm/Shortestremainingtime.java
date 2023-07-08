85dbd2116f752cc74e9c3d7d7c67ba224795907b
Fantiko <86378890+Fantiko@users.noreply.github.com>
2023-07-08 16:37:18 -0300

    Shortest remaining time

--- src/main/java/com/github/rok/algorithm/Shortestremainingtime.java
+++ src/main/java/com/github/rok/algorithm/Shortestremainingtime.java
@@ -7,20 +7,17 @@ import com.github.rok.os.Process;
 /*
  * @author Fantiko, Kaio Stefan Campos Nunes created on 07/07/2023
  */
-public class ShortestJobFirst implements AlgorithmInterface{
-
+public class Shortestremainingtime implements AlgorithmInterface{
     private final IMainController controller;
 
-    public ShortestJobFirst(IMainController controller) {
+    public Shortestremainingtime(IMainController controller) {
         this.controller = controller;
     }
 
     @Override
     public void execute() {
-        Process lowestSizeProcess = controller.getIMemory().getLowestTimeProcess();
-        if (lowestSizeProcess == null) {
-            return;
-        }
-        controller.addProcessToCPU(lowestSizeProcess, lowestSizeProcess.getWaitingTime()/2);
+        Process ShortestRemainingTime = controller.getIMemory().getLowestTimeProcess();
+        if (ShortestRemainingTime == null) return;
+        controller.addProcessToCPU(ShortestRemainingTime, ShortestRemainingTime.getWaitingTime()/2);
     }
 }
