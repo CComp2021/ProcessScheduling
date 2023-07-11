d2eea33c701a35d81e2d3a5c0039715e6f899474
Joao Victor <102428176+JvMSan@users.noreply.github.com>
2023-07-11 17:44:41 -0300

    roundRed

--- src/main/java/com/github/rok/algorithm/AlgorithmRoundRobin.java
+++ src/main/java/com/github/rok/algorithm/AlgorithmRoundRobin.java
@@ -10,35 +10,17 @@ import com.github.rok.os.Process;
 public class AlgorithmRoundRobin implements AlgorithmInterface {
     private final IMainController controller;
 
-    private static final double TIME_QUANTUM = 1.0; // Quantum de tempo (em segundos)
-
-    private Process currentProcess; // Processo atualmente em execução
-    private double remainingTime; // Tempo restante para o processo atual
+    private int pos = 0;
 
     public AlgorithmRoundRobin(IMainController controller) {
         this.controller = controller;
     }
 
-    @Override
     public void execute() {
-        if (currentProcess == null) {
-            // Verifica se há algum processo pronto para ser executado
-            Process nextProcess = controller.getIMemory().getLowestIdProcess();
-            if (nextProcess != null) {
-                currentProcess = nextProcess;
-                remainingTime = TIME_QUANTUM;
-                controller.addProcessToCPU(currentProcess, TIME_QUANTUM);
-            }
-        }else if(currentProcess == controller.getLastProcess()){
-            controller.addProcessToCPU(currentProcess,remainingTime);
-            currentProcess = controller.getIMemory().getLowestIdProcess();
-        } else {
-            Process nextProcess = controller.getIMemory().getProcess(currentProcess.getId()+1);
-            controller.addProcessToCPU(currentProcess,remainingTime);
-            currentProcess = nextProcess;
-            remainingTime = TIME_QUANTUM;
-            controller.addProcessToCPU(currentProcess,TIME_QUANTUM);
-        }
-
+        Process nextProcess = controller.getIMemory().getNextProcessOnList();
+        if(nextProcess == null)return;
+        controller.addProcessToCPU(nextProcess, controller.getTimeOnCpu());
+        pos++;
     }
 }
+
